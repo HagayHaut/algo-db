da4b4effaa8fb877de9e62829e8711b60dccd8e1
Hagay Haut <hagayhaut@gmail.com>
2022-06-27 19:03:55 -0400

    add comments to /logout and /login actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   skip_before_action :authorize, only: :create
 
+  # POST '/login'
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
@@ -11,6 +12,7 @@ class SessionsController < ApplicationController
     end
   end
 
+  # DELETE '/logout'
   def destroy
     session.delete :user_id
     head :no_content
