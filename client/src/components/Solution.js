3916a49fc6cd31b4e7ffb3c2440d6e2d3714d017
Hagay Haut <hagayhaut@gmail.com>
2022-06-29 19:02:48 -0400

    format code snippets with react-syntax-highlighter

--- client/src/components/Solution.js
+++ client/src/components/Solution.js
@@ -1,31 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import obsidian from "react-syntax-highlighter/dist/esm/styles/hljs/obsidian";
 
 const SolutionContainer = styled.div`
   border: 1px solid black;
 `;
 
-const Canvas = styled.div`
-  white-space: pre-wrap;
-`;
-
-const Line = styled.p`
-  margin: 0;
-  font-family: "Courier New", Courier, monospace;
-`;
-
 function Solution({ selectedSolution }) {
-  const { solution, time_complexity, space_complexity, challenge, notes } =
-    selectedSolution;
-
-  const displaySolution = solution
-    .split("\n")
-    .map((line, i) => <Line key={i}>{line}</Line>);
+  const {
+    solution,
+    time_complexity,
+    space_complexity,
+    challenge,
+    notes,
+    language,
+  } = selectedSolution;
 
   return (
     <SolutionContainer>
       <p>Solution for {challenge.title}</p>
-      <Canvas>{displaySolution}</Canvas>
+      <SyntaxHighlighter language={language} style={obsidian}>
+        {solution}
+      </SyntaxHighlighter>
     </SolutionContainer>
   );
 }
