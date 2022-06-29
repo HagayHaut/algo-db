6a9f0e62bb77505a2c1ac0563153820b7be34c8b
Hagay Haut <hagayhaut@gmail.com>
2022-06-29 10:47:55 -0400

    create AddSolution and AddChallenge forms

--- client/src/components/AddChallenge.js
+++ client/src/components/AddChallenge.js
@@ -1,7 +1,75 @@
 import React from "react";
+import styled from "styled-components";
+
+const FormContainer = styled.div`
+  border: 1px solid black;
+  padding: 20px;
+  width: 50%;
+  text-align: center;
+`;
+
+const Input = styled.input`
+  display: block;
+  height: 50%;
+  width: 100%;
+  text-align: center;
+`;
+
+const InputContainer = styled.div`
+  padding-top: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 200px;
+`;
+
+const SubmitButton = styled.button`
+  margin-top: 20px;
+`;
 
 function AddChallenge() {
-  return <div>AddChallenge</div>;
+  return (
+    <FormContainer>
+      <h2>Add a New Challenge</h2>
+      <form>
+        <InputContainer>
+          <label>Challenge Title</label>
+          <Input type="text"></Input>
+        </InputContainer>
+        <InputContainer>
+          <label>Challenge Description</label>
+          <TextArea></TextArea>
+        </InputContainer>
+        <InputContainer>
+          <label>External URL (optional)</label>
+          <Input type="text"></Input>
+        </InputContainer>
+        <InputContainer>
+          <label>Challenge Category</label>
+          <select>
+            <option></option>
+            <option>Array</option>
+            <option>Hash Map</option>
+            <option>Linked List</option>
+            <option>Binary Tree</option>
+            <option>Graph</option>
+            <option>Two Pointer</option>
+            <option>Sliding Window</option>
+            <option>Set</option>
+            <option>Sort</option>
+            <option>Stack/Queue</option>
+            <option>Recursion</option>
+            <option>String</option>
+          </select>
+        </InputContainer>
+
+        <SubmitButton type="submit">Submit Challenge</SubmitButton>
+      </form>
+    </FormContainer>
+  );
 }
 
 export default AddChallenge;
--- client/src/components/AddSolution.js
+++ client/src/components/AddSolution.js
@@ -1,7 +1,107 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const FormContainer = styled.div`
+  border: 1px solid black;
+  padding: 20px;
+  width: 50%;
+  text-align: center;
+`;
+
+const Input = styled.input`
+  display: block;
+  height: 50%;
+  width: 100%;
+  text-align: center;
+`;
+
+const InputContainer = styled.div`
+  padding-top: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 200px;
+`;
+
+const SubmitButton = styled.button`
+  margin-top: 20px;
+`;
 
 function AddSolution() {
-  return <div>AddSolution</div>;
+  const [allChallenges, setAllChallenges] = useState([]);
+
+  useEffect(() => {
+    getAllChallenges();
+  }, []);
+
+  async function getAllChallenges() {
+    const response = await fetch("/challenges");
+    const data = await response.json();
+    setAllChallenges(data);
+  }
+
+  const complexities = [
+    "O(1)",
+    "O(log-n)",
+    "O(n-log-n)",
+    "O(n)",
+    "O(n^2)",
+    "O(n!)",
+  ];
+
+  const challengeOptions = allChallenges.map((challenge) => (
+    <option key={challenge.id} value={challenge.id}>
+      {challenge.title}
+    </option>
+  ));
+
+  const complexityOptions = complexities.map((complexity) => (
+    <option key={complexity} value={complexity}>
+      {complexity}
+    </option>
+  ));
+
+  return (
+    <FormContainer>
+      <h2>Add a New Solution</h2>
+      <form>
+        <InputContainer>
+          <label>Pick Challenge</label>
+          <select>
+            <option></option>
+            {challengeOptions}
+          </select>
+        </InputContainer>
+        <InputContainer>
+          <label>Your Solution:</label>
+          <TextArea></TextArea>
+        </InputContainer>
+        <InputContainer>
+          <label>Time Complexity</label>
+          <select>
+            <option></option>
+            {complexityOptions}
+          </select>
+        </InputContainer>
+        <InputContainer>
+          <label>Space Complexity</label>
+          <select>
+            <option></option>
+            {complexityOptions}
+          </select>
+        </InputContainer>
+        <InputContainer>
+          <label>Solution Notes:</label>
+          <TextArea></TextArea>
+        </InputContainer>
+
+        <SubmitButton type="submit">Submit Solution</SubmitButton>
+      </form>
+    </FormContainer>
+  );
 }
 
 export default AddSolution;
