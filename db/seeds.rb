78c8319ba4838ae64702977af509cf2810877648
Hagay Haut <hagayhaut@gmail.com>
2022-06-29 21:58:24 -0400

    make seeding logs more clear

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-puts 'Seeding... 🌱'
+puts 'Begin seeding... 🌱'
 User.destroy_all
 Solution.destroy_all
 Challenge.destroy_all
