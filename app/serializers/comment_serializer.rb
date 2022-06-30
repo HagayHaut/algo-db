a11b50b1eb4c021d5e3a2707eaad590da543c8bd
Hagay Haut <hagayhaut@gmail.com>
2022-06-30 09:30:27 -0400

    serialize comments & solutions

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,6 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :solution_id, :comment
+  attributes :id, :solution_id, :comment, :user_id
+
+  belongs_to :user
+  belongs_to :solution
 end
--- app/serializers/solution_serializer.rb
+++ app/serializers/solution_serializer.rb
@@ -2,4 +2,5 @@ class SolutionSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :challenge_id, :solution, :time_complexity, :space_complexity, :notes, :language
 
   belongs_to :challenge
+  belongs_to :user
 end
