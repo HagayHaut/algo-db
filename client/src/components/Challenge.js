0993aacff84b472fdf96892ea2cfc765f342631c
Hagay Haut <hagayhaut@gmail.com>
2022-07-03 17:56:27 -0400

    update All Solutions title margin

--- client/src/components/Challenge.js
+++ client/src/components/Challenge.js
@@ -50,7 +50,7 @@ const SolutionListContainer = styled.div`
   overflow-y: auto;
   background-color: #222;
   overflow-x: hidden;
-  height: calc(100vh - 60px);
+  height: calc(100vh - 65px);
 `;
 
 const CloseButton = styled.button`
@@ -82,7 +82,7 @@ const AllSolutionsTitle = styled.p`
   text-align: center;
   font-size: 16;
   display: inline;
-  margin: 10px 10px 5px 12px;
+  margin: 10px 10px 10px 12px;
 `;
 
 const Divider = styled.div`
