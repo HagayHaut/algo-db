da8ab9ffbcd5f80e3d4e261dbcb916eef3e7ebc6
Hagay Haut <hagayhaut@gmail.com>
2022-07-17 15:31:08 -0400

    update usernmae display style in user list

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -19,17 +19,25 @@ const CountTitle = styled.p`
   display: relative;
   top: 3px;
   left: 3px;
-  font-size: 0.5rem;
+  font-size: 0.4rem;
+  color: #888;
 `;
 
 const Count = styled.p`
   text-align: center;
-  font-size: 0.8rem;
+  font-size: 0.7rem;
 `;
 
 const Joined = styled.div`
   width: 30%;
   height: 22px;
+  margin: 2px;
+`;
+
+const UsernameContainer = styled.div`
+  width: 25%;
+  height: 22px;
+  border-right: 1px solid black;
   margin: 2px;
 `;
 
@@ -39,17 +47,17 @@ function UserCard({ user, isMe }) {
   const color = isMe ? "#05d5fa" : "#bbb";
 
   const usernameStyle = {
-    width: "25%",
-    height: "22px",
-    borderRight: "1px solid black",
-    margin: "2px",
-    fontSize: "0.8rem",
+    textAlign: "center",
+    fontSize: "0.7rem",
     color,
   };
 
   return (
     <Card>
-      <p style={usernameStyle}>{username + (isMe ? " (me)" : "")}</p>
+      <UsernameContainer>
+        <CountTitle>Username</CountTitle>
+        <p style={usernameStyle}>{username + (isMe ? " (me)" : "")}</p>
+      </UsernameContainer>
       <CountsContainer>
         <CountTitle>Solutions</CountTitle>
         <Count>{counts.solution_count} </Count>
